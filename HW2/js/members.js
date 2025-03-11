b8a4ee2bd87afe2f8ccad7abee3898bfe122c928
Boaz Roskam <b.w.j.roskam.students@uu.nl>
2025-03-11 14:01:50 +0100

    Table gets put in dynamicly via javascript

--- HW2/js/members.js
+++ HW2/js/members.js
@@ -6,7 +6,6 @@ function addAllEventListeners() {
 //Get json string from storage and return as object
 function getObjectFromStorage(key) {
     let jsonString = sessionStorage.getItem(key);
-    console.log(jsonString);
     return JSON.parse(jsonString);
 }
 
@@ -14,37 +13,73 @@ function loadPageWithInformation(studentObject) {
     //Refrence to elements we need
     const header = document.getElementsByTagName("header")[0];
     const studentSection = document.getElementById("section-student");
-    const courseSection = document.getElementById("sectino-course");
+    const courseSection = document.getElementById("section-course");
+    const amountOfCourses = studentObject.courses.length;
 
     //Create all elements
-    let h1 = document.createElement("h1");
+    let section = document.createElement("section");
+    let h1El = document.createElement("h1");
     let h2 = document.createElement("h2");
-    let paragraph = document.createElement("p");
-    let table = document.createElement("table");
-    let tableHead = document.createElement("thead")
-    let TableBody = document.createElement("tbody")
-    let tableRow = document.createElement("tr")
-    let tableHeadText = document.createElement("th");
-    let courseTableRowText = document.createElement("td");
+    let paragraphEl = document.createElement("p");
+    let tableEl = document.createElement("table");
 
     //Put elements in
     header.appendChild(newText(`About ${studentObject.firstName} ${studentObject.lastName}`));
-
-    studentSection.appendChild(h1);
+    studentSection.appendChild(h1El);
     let studentSectionTitle = studentSection.getElementsByTagName("h1")[0];
     studentSectionTitle.appendChild(newText(`Who is ${studentObject.firstName} ${studentObject.lastName}?`))
 
-    studentSection.appendChild(paragraph);
+    studentSection.appendChild(paragraphEl);
     let studentSectionParagraph = studentSection.getElementsByTagName("p")[0];
     studentSectionParagraph.appendChild(newText(studentObject.firstName));
     studentSectionParagraph.appendChild(newText(studentObject.lastName));
+
+    //Creating course table
+    courseSection.appendChild(tableEl);
+    let table = courseSection.getElementsByTagName("table")[0];
+    //Creating the head
+    let TableHead = table.getElementsByTagName("thead")[0];
+    let tableRow = document.createElement("tr")
+    TableHead.appendChild(tableRow);
+    let courseTableHeadRow = TableHead.getElementsByTagName("tr")[0];
+    for (let i = 0; i < 2; i++) {
+        let tableHeadRowText = document.createElement("th");
+        courseTableHeadRow.appendChild(tableHeadRowText);
+    }
+    let courseTableHeadRowElements = courseTableHeadRow.getElementsByTagName("th");
+    courseTableHeadRowElements[0].appendChild(newText("Title"));
+    courseTableHeadRowElements[1].appendChild(newText("Teacher"));
+    courseTableHeadRowElements[2].appendChild(newText("Description"));
+
+    //Creating the body
+    tableBody = table.getElementsByTagName("tbody")[0];
+    for (let i = 0; i < amountOfCourses - 1; i++) {
+        let tableRow = document.createElement("tr")
+        tableBody.appendChild(tableRow);
+    }
+    let tableBodyRows = tableBody.getElementsByTagName("tr");
+    for (let i = 0; i < tableBodyRows.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            let tableBodyRowData = document.createElement("td");
+            if (i == 0 && j == 2) {
+                continue;
+            }
+            tableBodyRows[i].appendChild(tableBodyRowData);
+        }
+    }
+    //Filling in the values of the courses
+    for (let i = 0; i < tableBodyRows.length; i++) {
+        let tableBodyRowTexts = tableBodyRows[i].getElementsByTagName("td");
+        tableBodyRowTexts[0].appendChild(newText(studentObject.courses[i].title));
+        tableBodyRowTexts[1].appendChild(newText(studentObject.courses[i].teacher.firstName + " " + studentObject.courses[i].teacher.lastName));
+        tableBodyRowTexts[2].appendChild(newText(studentObject.courses[i].description));
+    }
 }
 
 //Creates a new text node
 function newText(text) {
     return document.createTextNode(text);
 }
-
 
 function onRun() {
     addAllEventListeners();
